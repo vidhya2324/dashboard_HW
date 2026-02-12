340c235410547eb3694dd35a129432093afc66b4
vidhya2324 <vidhyaengineer2022@gmail.com>
2026-02-12 14:00:01 +0530

    update the main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:dashboardca/auth/login_page.dart';
 import 'package:flutter/material.dart';
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
